3bbea430833709b7ef671006d4c85d0fc1d497c4
vamsimakke@gmail.com <vamsimakke@gmail.com>
2025-06-05 10:37:29 -0400

    Created a basic file structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Main {
+import file.entity.File;
+import file.entity.FileSystemEntity;
+import file.entity.Folder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Folder root=new Folder("root");
+
+
+        File file1= new File("File inside root");
+        Folder folder1=new Folder("subfolder inside root");
+        File file2= new File("File inside subfolder");
+        root.addChild(file1);
+        root.addChild(folder1);
+        folder1.addChild(file2);
+
+        dfs(root);
+
+
+    }
+
+    private static void dfs(FileSystemEntity root) {
+
+        if(root.isFile()){
+            System.out.println(root.getTitle());
+        }else{
+            Folder folder= (Folder) root;
+            System.out.println(folder.getTitle());
+            for(FileSystemEntity fileSystemEntity: folder.getChildren())
+                dfs(fileSystemEntity);
+        }
+    }
 }
